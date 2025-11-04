8fc0cfc3b064f6a25908b0f438b7ce50f1df8b6d
sam <samtao.coulet@gmail.com>
2025-11-04 13:41:40 +0100

    feat: wpscan function

---
+++ Wpscan/wpscan.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void wpscan_scan(void) {
+    char url[100];
+    char command[200];
+
+    printf("Enter the WordPress site URL (e.g., http://example.com): ");
+    scanf("%99s", url);
+
+    printf("Scanning WordPress site at %s...\n", url);
+
+    snprintf(command, sizeof(command), "wpscan --url %s --enumerate u", url);
+
+    system(command);
+}
---
+++ Wpscan/wpscan.h
@@ -0,0 +1,7 @@
+#ifndef WPSCNAN_H
+#define WPSCNAN_H       
+
+void wpscan_menu(void);
+void wpscan_scan(void);
+
+#endif
